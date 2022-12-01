20b46d0f4a63fd897d6bb2718c7a998ae0f1fef9
Ayoub Achak <ayoub.achak01@gmail.com>
2022-12-01 21:01:44 +0100

    Backend set

---
+++ src/test/java/parc/ParcApplicationTests.java
@@ -0,0 +1,13 @@
+package parc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
